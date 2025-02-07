1fecfe5367784d5bccdf543017c583a75e293087
ENG_BXI <eng.0.bxi@gmail.com>
2025-02-07 22:54:27 +0300

    create home , about , popular section and i have some error in swiper img pizza [WIP]

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*=============== SHOW MENU ===============*/
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+/*=============== SWIPER POPULAR ===============*/
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+
+});
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
