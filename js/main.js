2a3e0d3fd8fae127084ac8344bf9ada9db4980f1
ENG_BXI <eng.0.bxi@gmail.com>
2025-02-12 21:48:17 +0300

    complete pizza website withOut test

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ const swiper = new Swiper(".swiper", {
   grabCursor: true,
   slidesPerView: "auto",
   centeredSlides: "auto",
-
-
+  autoplay: {
+    delay: 3000,
+  },
 });
 
 /*=============== SHOW SCROLL UP ===============*/
@@ -19,3 +20,49 @@ const swiper = new Swiper(".swiper", {
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+var slideUp = {
+  opacity: 0.5,
+  duration: 2000,
+};
+
+var rotateImage = {
+  rotate: { z: 100 },
+};
+
+ScrollReveal().reveal(".home-data , .popular-container , .section-title", {
+  ...slideUp,
+  origin: "top",
+  distance: "20%",
+});
+ScrollReveal().reveal(".leaf", {
+  ...slideUp,
+  origin: "top",
+  distance: "50%",
+  opacity: 0,
+  delay: 1900,
+  duration: 2000,
+});
+ScrollReveal().reveal(
+  ".home-image , .about-data , img.recipe-image , .contact-data",
+  {
+    ...slideUp,
+    origin: "right",
+    distance: "50%",
+  }
+);
+ScrollReveal().reveal(".about-image  , .recipe-list , .content-img", {
+  ...slideUp,
+  origin: "left",
+  distance: "50%",
+});
+ScrollReveal().reveal(".product-item , .footer-container", {
+  ...slideUp,
+  distance: "50%",
+});
+ScrollReveal().reveal(".home-pizza ", {
+  ...slideUp,
+  ...rotateImage,
+  origin: "top",
+  distance: "10%",
+  duration: 2500,
+});
